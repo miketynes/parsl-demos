903bf6c3c6a5e8d60ec65167b7138b807ca66d74
miketynes <mtynes@uchicago.edu>
2024-05-01 11:35:02 -0500

    fix conda activation; print parsl env

--- improv/bag_of_tasks.py
+++ improv/bag_of_tasks.py
@@ -26,22 +26,29 @@ def random_sleep_task(
     """
     import time
     import random
+    import os
 
     sleep_duration = random.randint(sleep_min, sleep_max)
     time.sleep(sleep_duration)
 
+    parsl_env = {k: v for k, v in os.environ.items() if  'parsl' in k.lower()}
+    parsl_env = '\n'.join([f'{k}: {v}' for k, v in parsl_env.items()])
+
     # dont print hello, let the main thread do that
     # (we dont want to deal with stdout yet)
-    msg = f'Hello from task {task_ix} (slept for {sleep_duration})'
+    msg = f"""
+Hello from task {task_ix}.
+Parsl env:
+{parsl_env}
+Slept for {sleep_duration}(s).
+"""
     return msg
 
 if __name__ == "__main__": 
 
-
-    
     n_tasks = 10
-    min_sleep = 15
-    max_sleep = 30
+    sleep_min = 5
+    sleep_max = 10
 
     config = Config(
         executors=[
@@ -54,7 +61,10 @@ if __name__ == "__main__":
                     walltime='00:10:00',
                     init_blocks=1, 
                     max_blocks=1,
-                    worker_init='/home/mtynes/miniconda3/bin/activate parsl-demos',
+                    worker_init="""
+source activate parsl-demos
+which python
+""",
                     launcher=SimpleLauncher(),
                 ),
             )
@@ -67,8 +77,8 @@ if __name__ == "__main__":
     for task_ix in range(n_tasks):
         msg = random_sleep_task(
             task_ix=task_ix,
-            min_sleep=min_sleep,
-            max_sleep=max_sleep,
+            sleep_min=sleep_min,
+            sleep_max=sleep_max,
         )
         futures.append(msg)
     for future in tqdm(as_completed(futures), total=len(futures)):
